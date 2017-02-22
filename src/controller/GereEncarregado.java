8ed177ced6918fc3021cab41c163a73a22dce4dd
UserPL135Pc12 <UserPL135Pc12@SLFRPL135PC12.ensino.atec.local>
2017-02-22 21:59:34 +0000

    gere encarregado

---
+++ src/controller/GereEncarregado.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.util.ArrayList;
+import model.Encarregado;
+
+public class GereEncarregado 
+{
+	ArrayList <Encarregado> arEncarregado;
+	
+	public GereEncarregado()
+	{
+		arEncarregado = new ArrayList<>();
+		
+		public void addEncarregado(String nome, String morada, int cc, int nif, int contacto, int nib)
+		{
+			Encarregado e = new Encarregado(arEncarregado.size(), nome, morada, cc, nif, contacto, nib);
+		}
+	}
+}
